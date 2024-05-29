757b86f62fe9a04c79c84570fea3535ae473664b
Andrea-Morabito <andreamorabito05@gmail.com>
2024-05-29 22:24:51 +0200

    index page design

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expresso Shop</title>
+    <link rel="stylesheet" type="text/css" href="CoffeShop/src/style.css">
 </head>
 <body>
-    <p>Congratulation, you are on the "Expresso" shop</p>
+    <nav >
+        <img style="padding-bottom:3px" width="64px" heigth="64px" src="CoffeShop/src/img/company_logo.png" alt="logo">
+        <p class="logo-text danfo-regular">EXPRESSO</p>
+        <a href="#">Menu</a>
+        <a href="#">Chi siamo</a>
+    </nav>
+
+    <div style="display:flex;" class="center">
+        <div style="display:flex;" class="special-coffe-cover"></div>
+        <div class="special-coffe-container center" style="display:flex">
+            <img width="250px" heigth="250px" src="CoffeShop/src/img/special_coffe.png" alt="caffe_speciale">
+        </div>
+    </div>
 </body>
 </html>
