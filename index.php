ac93d0c7ba320025584501421128f1dab2efdd47
Andrea-Morabito <andreamorabito05@gmail.com>
2024-05-30 20:47:26 +0200

    main page devloping

--- index.php
+++ index.php
@@ -9,16 +9,20 @@
 <body>
     <nav >
         <img style="padding-bottom:3px" width="64px" heigth="64px" src="CoffeShop/src/img/company_logo.png" alt="logo">
-        <p class="logo-text danfo-regular">EXPRESSO</p>
-        <a href="#">Menu</a>
-        <a href="#">Chi siamo</a>
+        <p class="logo-text oleo-script-bold">EXPRESSO</p>
+        <a class="nav-link oleo-script-regular" href="#">Menu</a>
+        <a class="nav-link oleo-script-regular" href="#">Chi siamo</a>
     </nav>
 
-    <div style="display:flex;" class="center">
+    <div style="display:flex; margin: 1em 0" class="center">
         <div style="display:flex;" class="special-coffe-cover"></div>
         <div class="special-coffe-container center" style="display:flex">
             <img width="250px" heigth="250px" src="CoffeShop/src/img/special_coffe.png" alt="caffe_speciale">
         </div>
+    </div>
+
+    <div style="display:flex" class="footer center">
+        <p style="font-size: 20px" class="oleo-script-bold">@EXPRESSO</p>
     </div>
 </body>
 </html>
