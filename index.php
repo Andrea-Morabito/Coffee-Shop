c55b97a00a06eb4664054fd8ee55e05dc03cee1f
Andrea-Morabito <andreamorabito05@gmail.com>
2024-06-03 23:10:38 +0200

    finalized main page, TODO menu

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <nav >
         <img style="padding-bottom:3px" width="64px" heigth="64px" src="CoffeShop/src/img/company_logo.png" alt="logo">
         <p class="logo-text oleo-script-bold">EXPRESSO</p>
-        <a class="nav-link oleo-script-regular" href="#">Menu</a>
+        <a class="nav-link oleo-script-regular" href="./CoffeShop/menu.php">Menu</a>
         <a class="nav-link oleo-script-regular" href="#">Chi siamo</a>
     </nav>
 
@@ -21,14 +21,42 @@
         </div>
     </div>
 
-    <div class="heading-container">
-        <img src="CoffeShop/src/img/cafe-image.png" alt="">
-
+    <div style="margin: 2em 0;" class="text-center">
+        <h1 style="font-size:40px" class="lobster-regular">Benvenuti all' <strong>Expresso</strong></h1>
+        <p class="kanit-regular" style="font-size:20px">Dalle lontane montagne del Perù...</p>
+        <p class="kanit-regular" style="font-size:20px">Il bar <span class="lobster-regular">Expresso</span> offre da più di 500 anni il migior caffè del mondo</p>
+    </div>
+    
+    <h1 style="margin-top: 4em" class="text-center">Assapora i nostri prodotti</h1>
+    
+    <div style="margin: 5em 25%">
+        <div style="display:flex">
+            <img width="400px" src="CoffeShop/src/img/cookie-image.jpg" alt="">
+            <div style="margin-left:3em">
+                <h1 style="font-size: 40px; margin-bottom:1em;" class="lobster-regular">Biscotti</h1>
+                <p style="width:20em; style="font-size; 20px" class="kanit-regular">Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit. Eius, id? Nisi dolorum fugit, in veniam, alias dolorem tempore voluptas molestiae aspernatur magnam corrupti dolore cum ullam sunt. Nesciunt, in alias.</p>
+            </div>
+        </div>
     </div>
 
+
+    <div style="margin: 5em 30%">
+        <div style="display:flex">
+            <div style="margin-right:3em">
+                <h1 style="font-size: 40px; margin-bottom:1em;" class="lobster-regular">Cornetti</h1>
+                <p style="width:20em; font-size; 20px" class="kanit-regular">Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit. Eius, id? Nisi dolorum fugit, in veniam, alias dolorem tempore voluptas molestiae aspernatur magnam corrupti dolore cum ullam sunt. Nesciunt, in alias.</p>
+                </div>
+            <div>
+            <img width="400px" src="CoffeShop/src/img/croissant-image.jpg" alt="">
+            </div>
+            </div>
+    </div>
 
     <div style="display:flex" class="footer center">
         <p style="font-size: 20px" class="oleo-script-bold">@EXPRESSO</p>
     </div>
+
 </body>
 </html>
